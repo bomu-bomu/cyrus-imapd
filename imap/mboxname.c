e9ad73e7bbaa4f9ed3f500b5b5dc341dd30055c8
Ken Murchison <murch@fastmail.com>
2019-04-17 09:18:20 -0400

    Allow read/write of mailboxes using legacy (by name) directories based on the MBTYPE_LEGACY_DIRS flag

--- imap/mboxname.c
+++ imap/mboxname.c
@@ -1880,7 +1880,7 @@ EXPORTED char *mboxname_datapath(const char *partition,
     static char pathresult[MAX_MAILBOX_PATH+1];
     const char *root;
 
-    if (!partition || !uniqueid) return NULL;
+    if (!partition) return NULL;
 
     root = config_partitiondir(partition);
     if (!root) return NULL;
@@ -1890,7 +1890,14 @@ EXPORTED char *mboxname_datapath(const char *partition,
         return pathresult;
     }
 
-    mboxname_id_hash(pathresult, MAX_MAILBOX_PATH, root, uniqueid);
+    if (uniqueid) {
+        /* Mailbox dir by uniqueid */
+        mboxname_id_hash(pathresult, MAX_MAILBOX_PATH, root, uniqueid);
+    }
+    else {
+        /* Legacy mailbox dir by mboxname */
+        mboxname_hash(pathresult, MAX_MAILBOX_PATH, root, mboxname);
+    }
 
     if (uid) {
         int len = strlen(pathresult);
@@ -1913,7 +1920,7 @@ EXPORTED char *mboxname_archivepath(const char *partition,
     static char pathresult[MAX_MAILBOX_PATH+1];
     const char *root;
 
-    if (!partition || !uniqueid) return NULL;
+    if (!partition) return NULL;
 
     root = config_archivepartitiondir(partition);
     if (!root) root = config_partitiondir(partition);
@@ -1926,7 +1933,14 @@ EXPORTED char *mboxname_archivepath(const char *partition,
         return pathresult;
     }
 
-    mboxname_id_hash(pathresult, MAX_MAILBOX_PATH, root, uniqueid);
+    if (uniqueid) {
+        /* Mailbox dir by uniqueid */
+        mboxname_id_hash(pathresult, MAX_MAILBOX_PATH, root, uniqueid);
+    }
+    else {
+        /* Legacy mailbox dir by mboxname */
+        mboxname_hash(pathresult, MAX_MAILBOX_PATH, root, mboxname);
+    }
 
     if (uid) {
         int len = strlen(pathresult);
@@ -1983,7 +1997,7 @@ EXPORTED char *mboxname_metapath(const char *partition,
     const char *filename = NULL;
     char confkey[256];
 
-    if (!partition || !uniqueid) return NULL;
+    if (!partition) return NULL;
 
     *confkey = '\0';
 
@@ -2057,7 +2071,14 @@ EXPORTED char *mboxname_metapath(const char *partition,
         return metaresult;
     }
 
-    mboxname_id_hash(metaresult, MAX_MAILBOX_PATH, root, uniqueid);
+    if (uniqueid) {
+        /* Mailbox dir by uniqueid */
+        mboxname_id_hash(metaresult, MAX_MAILBOX_PATH, root, uniqueid);
+    }
+    else {
+        /* Legacy mailbox dir by mboxname */
+        mboxname_hash(metaresult, MAX_MAILBOX_PATH, root, mboxname);
+    }
 
     if (filename) {
         int len = strlen(metaresult);
